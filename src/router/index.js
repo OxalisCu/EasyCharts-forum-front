14959b64ce692eaba8b4dc2a30bd661a0a0f9898
OxalisCu <2127298698@qq.com>
2022-04-02 09:37:22 +0800

    在路由跳转之前，清除所有请求

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,16 @@
 import { createRouter, createWebHashHistory} from 'vue-router';
 import routes from './router.config';
+import {clearPending} from "../api/axios";
 
-export const router = createRouter({
+const router = createRouter({
   history: createWebHashHistory(),
   routes
 });
+
+// 在路由跳转之前，清除所有请求
+router.beforeEach((to, from, next) => {
+  clearPending()
+  next()
+})
+
+export default router
