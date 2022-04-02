9ad13cd7c954e8abe2a0aaa1f0a68f30cad2bd99
OxalisCu <2127298698@qq.com>
2022-04-02 19:46:08 +0800

    将reactive用ref代替，解决reactive数组重新赋值失去响应性问题

--- src/store/topic.js
+++ src/store/topic.js
@@ -1,27 +1,28 @@
 import {defineStore} from "pinia"
 import {getTopicList, getTopicDetail} from "../api/index.js"
-import {reactive} from "vue"
+import {ref} from "vue"
 
 export const useTopicStore = defineStore('topic', function() {
-  let topicList = reactive([])
+  let topicList = ref([])
 
   // 获取 topicList
   async function getList() {
     const res = await getTopicList()
-    topicList = reactive(res.data.data.topiclist)
+    topicList.value = res.data.data.topiclist
     return topicList
   }
 
   // 根据 topicId 获取 topicItem
   async function getDetail(topicId) {
     // 从 list 中找，找到返回
-    let topic = reactive(topicList.find((item) => (item.topicId === topicId)))
-    if(topic) {
+    let topic = ref()
+    topic.value = topicList.value.find((item) => (item.topicId === topicId))
+    if(topic.value) {
       return topic
     }
     // 没找到重新请求并返回
     const res = await getTopicDetail(topicId)
-    topic = reactive(res.data.data.topicdetail)
+    topic.value = res.data.data.topicdetail
     return topic
   }
 
