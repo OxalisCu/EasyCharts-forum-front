e6a0c9806211bb13cce3de76882def0fca2124c8
OxalisCu <2127298698@qq.com>
2022-04-02 11:51:36 +0800

    公告卡片

--- src/components/broadcard/BroadCard.vue
+++ src/components/broadcard/BroadCard.vue
@@ -1,50 +1,93 @@
 <script setup>
+import BroadItem from './BroadItem.vue'
+import {reactive} from "vue";
 
+const broadList = reactive([
+  {
+    "postid": 6006,
+    "title": "没我族效",
+    "content": "风油院张质十改理置代但保与究不便界志。研便设准听究而路放度用共儿。放山石队做县公线动见任队领型程东。引领质况南别所红上清前效表更线。",
+    "time": 1443953078547,
+    "viewnum": 37,
+    "commentnum": 26,
+    "ismark": 1,
+    "isnotice": 0,
+    "usermsg": {
+      "userid": 6007,
+      "name": "郝强",
+      "avatar": "http://xdpg.aero/fgjbvvlbu"
+    },
+    "topicmsg": {
+      "topicid": 6008,
+      "title": "面运系断位真",
+      "desc": "安价北需选林单任广空七信识用。",
+      "postnum": 21
+    }
+  },
+  {
+    "postid": 6009,
+    "title": "关离段得克明",
+    "content": "共照太山组向建化义此原战团教当队。成华六不属毛整率清对代易如在新每层。所清军细文等心展张划立队活。利各单三员素家图五马成容外同次五。路第引才美干北维开公压准。想华张那报且传然系究难路样长记我。",
+    "time": 692214725761,
+    "viewnum": 23,
+    "commentnum": 74,
+    "ismark": 0,
+    "isnotice": 1,
+    "usermsg": {
+      "userid": 6010,
+      "name": "唐勇",
+      "avatar": "http://pnojiqm.mg/ffpcrx"
+    },
+    "topicmsg": {
+      "topicid": 6011,
+      "title": "设七斗经长走",
+      "desc": "角感来基利志规议可展料问率间。",
+      "postnum": 73
+    }
+  },
+])
 </script>
 
 <template>
   <div class="broad-card card-hover card-border">
     <div class="tip">
-      <Icon icon="ri:broadcast-fill"></Icon>
+      <Icon class="icon" icon="ri:broadcast-fill"></Icon>
       <span>公告</span>
     </div>
     <div class="broad-list">
-      <div class="item">
-        <div class="title">
-          新人反馈指南
-        </div>
-        <el-row class="msg">
-          <el-col class="username" :span="12">
-            酢浆草Cu
-          </el-col>
-          <el-col class="post-time" :span="12">
-            2019-12-01
-          </el-col>
-        </el-row>
-      </div>
+      <BroadItem
+        class="broad-item"
+        v-for="item in broadList"
+        :key="item.postid"
+        :broadDetail="item"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
 .broad-card {
-  box-sizing: border-box;
   padding: 10px 20px;
-  background-color: #fff;
-  width: 100%;
 }
 
 .tip {
-  line-height: 40px;
-  border-bottom: 1px solid #999;
-}
-
-.item {
   padding: 10px 0;
-  border-radius: 5px;
+  border-bottom: 1px solid #999;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
 }
 
-.item:hover {
-  background-color: #eee;
+.tip .icon {
+  font-size: 18px;
+  margin-right: 5px;
+}
+
+.broad-list {
+  padding-top: 10px;
+}
+
+.broad-item {
+  margin-bottom: 5px;
 }
 </style>
---
+++ src/components/broadcard/BroadItem.vue
@@ -0,0 +1,56 @@
+<script setup>
+import {toRefs} from "vue";
+
+const props = defineProps({
+  broadDetail: Object
+})
+
+const {broadDetail} = toRefs(props)
+console.log('broadDetail', broadDetail)
+</script>
+
+<template>
+  <div class="broad-item">
+    <div class="title">
+      {{broadDetail.title}}
+    </div>
+    <el-row class="msg">
+      <el-col class="username" :span="12">
+        {{broadDetail.usermsg.name}}
+      </el-col>
+      <el-col class="post-time" :span="12">
+        {{broadDetail.time}}
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+.broad-item {
+  padding: 5px 5px;
+  border-radius: 5px;
+}
+
+.broad-item:hover {
+  background-color: #eee;
+}
+
+.title {
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+.msg {
+  color: #666;
+}
+
+.username {
+  overflow: hidden;
+  text-overflow:ellipsis;
+  white-space: nowrap;
+}
+
+.post-time {
+  text-align: right;
+}
+</style>
