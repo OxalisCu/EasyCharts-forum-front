a9cc7ca253133f1a6f1ad78ab99621007451c947
OxalisCu <2127298698@qq.com>
2022-04-04 01:10:16 +0800

    小Logo组件

---
+++ src/components/logo/LogoSimple.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="logo-simple">
+    EC
+  </div>
+</template>
+
+<style scoped>
+.logo-simple {
+  height: 40px;
+  line-height: 40px;
+  font-size: 24px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.logo-simple::selection {
+  color: #333;
+  background-color: #fff;
+}
+</style>
