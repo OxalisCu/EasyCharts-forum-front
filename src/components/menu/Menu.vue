1b775efa118bfad06db87a33096911b99f0a976b
OxalisCu <2127298698@qq.com>
2022-04-04 01:11:25 +0800

    菜单栏响应式布局

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,41 +1,93 @@
 <script setup>
 import Logo from '../logo/Logo.vue'
+import LogoSimple from '../logo/LogoSimple.vue'
 import SearchBox from "../searchbox/SearchBox.vue"
-import UserPre from "../userpre/UserPre.vue"
-import MsgBox from "../msgbox/MsgBox.vue"
+import SearchBoxSimple from "../searchbox/SearchBoxSimple.vue"
+import UserPre from "./UserPre.vue"
+import MsgBox from "./MsgBox.vue"
 
-const isLogin = true;
 </script>
 
 <template>
   <el-row class="menu" justify="center" align="middle">
-    <el-col :span="22">
-      <el-row justify="space-between" align="middle">
-        <el-col :span="3">
-          <Logo />
-        </el-col>
-        <el-col :span="8">
-          <SearchBox />
-        </el-col>
-        <el-col :span="3">
-          <el-row justify="space-evenly" align="bottom">
-            <el-col :span="10">
-              <el-row justify="center">
-                <MsgBox />
-              </el-row>
-            </el-col>
-            <el-col :span="10">
-              <el-row justify="center">
-                <UserPre />
-              </el-row>
-            </el-col>
-          </el-row>
-        </el-col>
-      </el-row>
+    <el-col class="menu-content" :span="22">
+      <div class="left">
+        <Logo class="logo" />
+        <LogoSimple class="logo-simple" />
+      </div>
+      <div class="search-box">
+        <SearchBox />
+      </div>
+      <div  class="search-box-simple">
+        <SearchBoxSimple />
+      </div>
+      <div class="right">
+        <MsgBox class="msg-box" />
+        <UserPre />
+      </div>
     </el-col>
   </el-row>
 </template>
 
 <style scoped>
+.menu-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+.right {
+  width: 120px;
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+}
+
+.msg-box {
+  margin-bottom: 2px;
+}
+
+.logo {
+  display: flex;
+}
+
+.logo-simple {
+  display: none;
+}
+
+.search-box {
+  display: flex;
+  width: 40%;
+  max-width: 500px;
+}
+
+.search-box-simple {
+  display: none;
+}
+
+@media screen and (max-width: 650px) {
+  .search-box {
+    display: none;
+  }
+  .search-box-simple {
+    display: flex;
+    position: absolute;
+    right: 180px;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .logo {
+    display: none;
+  }
+  .logo-simple {
+    display: flex;
+  }
+  .right {
+    width: 100px;
+  }
+  .search-box-simple {
+    right: 150px;
+  }
+}
 </style>
