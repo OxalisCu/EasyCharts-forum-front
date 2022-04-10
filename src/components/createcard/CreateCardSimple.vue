b40c5610d7680dac779ee6de2a0c1c5f08db9cd7
OxalisCu <2127298698@qq.com>
2022-04-10 23:58:30 +0800

    创建帖子按钮简化版
    (cherry picked from commit e582efdd6d6481fee97e6508a69eec931ba6f251)

---
+++ src/components/createcard/CreateCardSimple.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="create-card-simple click-btn">
+    <Icon class="icon" icon="system-uicons:write"></Icon>
+  </div>
+</template>
+
+<style scoped>
+.create-card-simple {
+  border-radius: 50%;
+  height: 50px;
+  width: 50px;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.icon {
+  font-size: 28px;
+  color: #666;
+}
+</style>
